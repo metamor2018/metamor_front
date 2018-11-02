5023f297c9022ccbff3fa1682e873ad39e896ae8
mogumannto <mogus.workshop@gmail.com>
2018-11-02 18:17:10 +0900

    Topページ用意

---
+++ src/views/Top.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="Top">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Top',
+  data() {
+    return {
+      
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
