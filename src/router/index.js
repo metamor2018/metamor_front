fb0acc4f7f458460cec4d56f6c0d7c609e559acb
mogumannto <mogus.workshop@gmail.com>
2018-11-02 19:29:30 +0900

    auth認証機能の実装

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+// views
 import Top from '@/views/Top';
+// components
+import Callback from '@/components/callback';
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'top',
       component: Top,
+    },
+    {
+      path: '/callback',
+      component: Callback,
     },
   ],
 });
--- src/views/Top.vue
+++ src/views/Top.vue
@@ -1,16 +1,32 @@
 <template>
   <div id="Top">
-
+    <nav class="navbar navbar-default">
+      <div class="navbar-header"></div>
+      <ul class="nav navbar-nav navbar-right">
+        <li>
+          <button class="btn btn-danger log" v-show="isLoggedIn()" @click="handleLogout()">Log out </button>
+          <button class="btn btn-info log" v-show="!isLoggedIn()" @click="handleLogin()">Log In</button>
+        </li>
+      </ul>
+    </nav>
   </div>
 </template>
 
 <script>
+import { isLoggedIn, login, logout } from '../../utils/auth';
+
 export default {
-  name: 'Top',
-  data() {
-    return {
-      
-    };
+  name: 'top',
+  methods: {
+    handleLogin() {
+      login();
+    },
+    handleLogout() {
+      logout();
+    },
+    isLoggedIn() {
+      return isLoggedIn();
+    },
   },
 };
 </script>
