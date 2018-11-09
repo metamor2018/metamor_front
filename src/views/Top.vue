622910097cbdff95bf3180ffb8e550092a1f8032
mogumannto <mogus.workshop@gmail.com>
2018-11-09 18:00:54 +0900

    TOPページ作成

--- src/views/Top.vue
+++ src/views/Top.vue
@@ -1,28 +1,29 @@
 <template>
-  <div id="Top">
-    <nav class="navbar navbar-default">
-      <div class="navbar-header"></div>
-      <ul class="nav navbar-nav navbar-right">
-        <li>
-          <button class="btn btn-danger log" v-show="isLoggedIn()" @click="handleLogout()">Log out </button>
-          <button class="btn btn-info log" v-show="!isLoggedIn()" @click="handleLogin()">Log In</button>
-        </li>
-      </ul>
-    </nav>
+  <div id="top">
+    <div class="container-fulid">
+      <div class="row">
+        <div class="col-sm left-box">
+          <h1 class="title-logo">Metamor</h1>
+        </div>
+        <div class="col-sm right-box">
+          <div class="login-btn">
+            <p>なりきり</p>
+            <button class="btn btn-info btn-lg" @click="handleLogin()">LOGIN</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import { isLoggedIn, login, logout } from '../../utils/auth';
+import { isLoggedIn, login } from '../../utils/auth';
 
 export default {
   name: 'top',
   methods: {
     handleLogin() {
       login();
-    },
-    handleLogout() {
-      logout();
     },
     isLoggedIn() {
       return isLoggedIn();
@@ -32,5 +33,44 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css?family=Kumar+One+Outline');
+
+#top{
+  height: 100%;
+  .container-fulid, .row, .col-sm{
+    height: 100%;
+  }
+  .title-logo, .login-btn{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    -ms-transform: translate(-50%,-50%);
+    -webkit-transform: translate(-50%,-50%);
+    transform: translate(-50%,-50%);
+    text-align: center;
+  }
+
+  .left-box{
+    background: #ffc80780;
+    .title-logo{
+      font-family: 'Kumar One Outline', cursive;
+      text-align: right;
+      font-size: 5rem;
+    }
+  }
+
+  .login-btn{
+    .btn{
+      width: 200px;
+      letter-spacing: 0;
+      transition: all 0.3s ease 0s;
+
+      &:hover{
+        width: 250px;
+        letter-spacing: 5px;
+      }
+    }
+  }
+}
 </style>
