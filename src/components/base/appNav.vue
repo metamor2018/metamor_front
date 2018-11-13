0f6f20224655ff7d2c45425c8daa73962f664d53
mogumannto <mogus.workshop@gmail.com>
2018-11-13 17:50:02 +0900

    ナビゲーションバー追加

---
+++ src/components/base/appNav.vue
@@ -0,0 +1,49 @@
+<template>
+  <b-navbar toggleable="md" type="dark" variant="info">
+
+    <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+    <b-collapse is-nav id="nav_collapse">
+
+      <b-navbar-nav>
+        <b-nav-item href="#">Test</b-nav-item>
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+
+        <!-- <b-nav-form>
+          <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search"/>
+          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+        </b-nav-form> -->
+
+
+        <b-nav-item-dropdown right>
+          <template slot="button-content">
+            <em>User</em>
+          </template>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item @click="handleLogout" style="color: red;">ログアウト</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+import { login, logout } from '../../../utils/auth';
+
+export default {
+  methods: {
+    // loginする
+    handleLogin() {
+      login();
+    },
+    // logoutする
+    handleLogout() {
+      logout();
+    }
+  },
+}
+</script>
