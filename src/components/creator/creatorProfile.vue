2908ad1b495250c42e7aebb39185ca0d89781b53
agoetc <b6151@oic.jp>
2019-01-06 23:51:29 +0900

    [update] profileをバックエンドから取得できるようにした

--- src/components/creator/creatorProfile.vue
+++ src/components/creator/creatorProfile.vue
@@ -5,17 +5,40 @@
         <img src="https://itc.moe/assets/images/avatar_b6232.png" alt="icon">
       </div>
       <div class="name">
-        <p class="creator_name">Hack-Man</p>
-        <p class="creator_id">@mogus_workshop</p>
+        <p class="creator_name">{{ creator.name }}</p>
+        <p class="creator_id">@{{ creator.id }}</p>
       </div>
       <div class="profile">
         <p>
-          test test test test test test test test test test test test test test
+          {{ creator.profile }}
         </p>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+import { findCreator } from '../../../utils/apis/creator';
+
+export default {
+  data() {
+    return {
+      creator: '',
+    };
+  },
+  mounted() {
+    this.$nextTick(() => {
+      findCreator(this.$route.params.id)
+        .then((data) => {
+          this.creator = data.data;
+        }).catch(() => {
+          this.$router.push({ path: '/error' });
+        });
+    });
+  },
+};
+
+</script>
 
 <style lang="scss" scoped>
 #creator-profile{
--- utils/apis/creator.js
+++ utils/apis/creator.js
@@ -1,8 +1,19 @@
 import axios from './axiosBase';
 
 /**
- * 創作者の作成
+ * 創作者の取得
+ * @param form
+ * @returns {AxiosPromise<any>}
  */
-export default function create(form) {
+export function create(form) {
   return axios.post('/creator', form);
+}
+
+/**
+ * 創作者の取得
+ * @param creatorId
+ * @returns {AxiosPromise<any>}
+ */
+export function findCreator(creatorId) {
+  return axios.get(`/creator/${creatorId}`);
 }
